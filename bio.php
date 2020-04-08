2c75c437147ed6f004a45f959da2e636c8f93bbd
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-04-08 16:30:30 +0700

    Membuat Biodata diri

---
+++ bio.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Nama         : Imam Alfarizi Syahputra <br>"; 
+    echo "Kelas        : XI RPL 2 <br>";
+    echo "Sekolah      : SMK Taruna Bhakti  <br>";
+    echo "Alamat Rumah : Jalan Kenanga Baru (kp Bojong) Depok, Jawa Barat <br>"
+?>
