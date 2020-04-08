341b3c702f48132a7bf41e219f55ddfd0514a8a1
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-04-08 16:38:01 +0700

    Menghapus Alamat, menambahkan No HP & Email

--- bio.php
+++ bio.php
@@ -2,5 +2,6 @@
     echo "Nama         : Imam Alfarizi Syahputra <br>"; 
     echo "Kelas        : XI RPL 2 <br>";
     echo "Sekolah      : SMK Taruna Bhakti  <br>";
-    echo "Alamat Rumah : Jalan Kenanga Baru (kp Bojong) Depok, Jawa Barat <br>"
+    echo "No Handphone : 0895422647836 <br>";
+    echo "Email        : imam.alfarizi.777@gmail.com <br>";
 ?>
